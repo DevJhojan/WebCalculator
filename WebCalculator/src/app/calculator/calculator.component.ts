797623ac5ed5479e6855f2b5d74385d23ec1a9f7
jdtp125753 <jhojan1806@gmail.com>
2023-04-19 17:14:31 -0500

    Added Features

--- WebCalculator/src/app/calculator/calculator.component.ts
+++ WebCalculator/src/app/calculator/calculator.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'calculator',
   templateUrl: './calculator.component.html',
   styleUrls: ['./calculator.component.scss']
 })
-export class CalculatorComponent {
+export class CalculatorComponent implements OnInit{
+  answer : string;
+  finish : boolean;
+  constructor (){
+    this.answer = '';
+    this.finish = false;
+  }
 
+  ngOnInit(): void { }
+
+  onAddkey($event: any) {
+    if(this.finish == true){
+      this.answer = $event.target.innerText;
+      this.finish = false;
+    }else{
+      this.answer += $event.target.innerText;
+    }
+
+
+  }
+
+  onAnswer(){
+    this.answer = eval(this.answer);
+    this.finish = true;
+  }
 }
